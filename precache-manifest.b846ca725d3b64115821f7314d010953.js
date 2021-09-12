69f06131708468695ed10eb1cc5585519bd9c6cf
Madeline Reiff <81939293+madelinekreiff@users.noreply.github.com>
2021-09-11 19:47:33 -0700

    Update precache-manifest.b846ca725d3b64115821f7314d010953.js
    Updated file paths to new repo name

--- precache-manifest.b846ca725d3b64115821f7314d010953.js
+++ precache-manifest.b846ca725d3b64115821f7314d010953.js
@@ -1,22 +1,22 @@
 self.__precacheManifest = [
   {
     "revision": "30a03993c45469c0067e",
-    "url": "https://madelinekreiff.github.io/csb-606wb/static/css/main.74628f81.chunk.css"
+    "url": "https://madelinekreiff.github.io/react-sticky-notes/static/css/main.74628f81.chunk.css"
   },
   {
     "revision": "30a03993c45469c0067e",
-    "url": "https://madelinekreiff.github.io/csb-606wb/static/js/main.c6392cdd.chunk.js"
+    "url": "https://madelinekreiff.github.io/react-sticky-notes/static/js/main.c6392cdd.chunk.js"
   },
   {
     "revision": "40b868f521eb2aa647b0",
-    "url": "https://madelinekreiff.github.io/csb-606wb/static/js/runtime~main.fefc06db.js"
+    "url": "https://madelinekreiff.github.io/react-sticky-notes/static/js/runtime~main.fefc06db.js"
   },
   {
     "revision": "09fc09cca39aeb28cc7e",
-    "url": "https://madelinekreiff.github.io/csb-606wb/static/js/2.28d8c36f.chunk.js"
+    "url": "https://madelinekreiff.github.io/react-sticky-notes/static/js/2.28d8c36f.chunk.js"
   },
   {
     "revision": "ce69b32eb89f4b94a5107a30513a093d",
-    "url": "https://madelinekreiff.github.io/csb-606wb/index.html"
+    "url": "https://madelinekreiff.github.io/react-sticky-notes/index.html"
   }
 ];
